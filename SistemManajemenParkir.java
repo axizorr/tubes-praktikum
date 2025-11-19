ce02704cfad299ee873bdcb028e3c1f9b16363c9
axizorr <126168421+axizorr@users.noreply.github.com>
2025-11-19 19:29:32 +0700

    file utama

---
+++ SistemManajemenParkir.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SistemManajemenParkir {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+    static void Menu(){
+        System.out.println("===== Sistem Manajemen Parkir =====");
+        System.out.println("1. Masuk Parkir");
+        System.out.println("2. Keluar Parkir");
+        System.out.println("3. Tampilkan Status Parkir");
+        System.out.println("4. Keluar Aplikasi");
+        System.out.print("Pilih menu: ");
+    }
+}
+
+
+
+
